c91841f6b9353ae4fe30cdf6b59554824f3e1539
mikejackson <michael.jackson@digital.justice.gov.uk>
2018-03-29 12:00:24 +0100

    Swagger doc stuff

--- src/main/java/uk/gov/justice/digital/nomis/controller/ChargesController.java
+++ src/main/java/uk/gov/justice/digital/nomis/controller/ChargesController.java
@@ -29,7 +29,7 @@ import java.util.Optional;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @RestController
-@Api( description = "Offender charge resources", tags = "Offender Chargges")
+@Api( description = "Offender charge resources", tags = "Offender Charges")
 public class ChargesController {
 
     private final OffenderChargesService offenderChargesService;
