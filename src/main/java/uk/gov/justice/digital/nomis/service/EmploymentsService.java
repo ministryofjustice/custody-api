82d6e202f85514bbf7a7455d7a6d4d64af1d3af0
mikejackson <michael.jackson@digital.justice.gov.uk>
2018-07-02 11:33:22 +0100

    Flatmap / sorting issue

--- src/main/java/uk/gov/justice/digital/nomis/service/EmploymentsService.java
+++ src/main/java/uk/gov/justice/digital/nomis/service/EmploymentsService.java
@@ -34,8 +34,8 @@ public class EmploymentsService {
         return Optional.ofNullable(offenderRepository.findOne(offenderId))
                 .map(offender -> offender.getOffenderBookings().stream()
                         .flatMap(offenderBooking -> offenderBooking.getOffenderEmployments().stream()
-                                .map(employmentsTransformer::employmentOf)
-                                .sorted(BY_EMPLOYMENT_SEQUENCE))
+                                .map(employmentsTransformer::employmentOf))
+                                .sorted(BY_EMPLOYMENT_SEQUENCE)
                         .collect(Collectors.toList()));
     }
 
