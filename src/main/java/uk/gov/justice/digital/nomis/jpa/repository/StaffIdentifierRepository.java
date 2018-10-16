9040b6fa69eead7de505c6408b0de4582db7b681
Michael Willis <michael.willis@digital.justice.gov.uk>
2018-10-16 16:53:06 +0100

    Change to JpaRepository

--- src/main/java/uk/gov/justice/digital/nomis/jpa/repository/StaffIdentifierRepository.java
+++ src/main/java/uk/gov/justice/digital/nomis/jpa/repository/StaffIdentifierRepository.java
@@ -1,11 +1,9 @@
 package uk.gov.justice.digital.nomis.jpa.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import uk.gov.justice.digital.nomis.jpa.entity.StaffIdentifier;
 import uk.gov.justice.digital.nomis.jpa.entity.StaffIdentifierIdentity;
 
-public interface StaffIdentifierRepository extends CrudRepository<StaffIdentifier, StaffIdentifierIdentity> {
-
-    StaffIdentifier findById_TypeAndId_IdentificationNumber(String type, String identificationNumber);
+public interface StaffIdentifierRepository extends JpaRepository<StaffIdentifier, StaffIdentifierIdentity> {
 
 }
--- src/main/java/uk/gov/justice/digital/nomis/jpa/repository/StaffRepository.java
+++ src/main/java/uk/gov/justice/digital/nomis/jpa/repository/StaffRepository.java
@@ -1,8 +1,8 @@
 package uk.gov.justice.digital.nomis.jpa.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import uk.gov.justice.digital.nomis.jpa.entity.Staff;
 
-public interface StaffRepository extends CrudRepository<Staff, Long> {
+public interface StaffRepository extends JpaRepository<Staff, Long> {
 
 }
