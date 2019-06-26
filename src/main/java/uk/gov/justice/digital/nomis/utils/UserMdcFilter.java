914b18ab47925d3a356ab953b83aa52a8d3cf50a
Peter Phillips <peter@greenthistle.co.uk>
2019-06-26 23:46:15 +0100

    DT-66: Correct oracle jar path

--- src/main/java/uk/gov/justice/digital/nomis/utils/UserMdcFilter.java
+++ src/main/java/uk/gov/justice/digital/nomis/utils/UserMdcFilter.java
@@ -4,43 +4,35 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.MDC;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 import static uk.gov.justice.digital.nomis.utils.MdcUtility.USER_ID_HEADER;
 
 @Slf4j
 @Component
 @Order(1)
-public class UserMdcFilter implements Filter {
+public class UserMdcFilter extends OncePerRequestFilter {
     @Override
-    public void init(final FilterConfig filterConfig) {
-        // Initialise - no functionality
-    }
-
-    @Override
-    public void doFilter(final ServletRequest request, final ServletResponse response, final FilterChain chain)
-            throws IOException, ServletException {
-
-        final var currentUsername = getUser((HttpServletRequest) request);
+    protected void doFilterInternal(final HttpServletRequest request, final HttpServletResponse response,
+                                    final FilterChain filterChain) throws ServletException, IOException {
+        final var currentUsername = getUser(request);
 
         try {
             if (currentUsername != null) {
                 MDC.put(USER_ID_HEADER, currentUsername);
             }
-            chain.doFilter(request, response);
+            filterChain.doFilter(request, response);
         } finally {
             if (currentUsername != null) {
                 MDC.remove(USER_ID_HEADER);
             }
         }
-    }
-
-    @Override
-    public void destroy() {
-        // Destroy - no functionality
     }
 
     private String getUser(final HttpServletRequest req) {
